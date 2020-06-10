45db7edc6d14d6d13c443fb424f5a6cff78ba2e8
Farhan Ahmed <Farhan.ahmed5160@gmail.com>
2020-06-11 01:44:18 +0500

    Add

---
+++ src/test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dinner(props) {
+    return (
+        <div>
+            <p>We are serving {props.dish}</p>
+        </div>
+    )
+}
+
+export default Dinner;
